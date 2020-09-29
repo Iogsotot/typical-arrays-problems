84d2b37228bf1ad1f3b8dcca23fe86aa6086acc7
Iogsotot <ajustusa@gmail.com>
2020-09-29 21:15:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length || typeof array == 'undefined') return 0;
+  let min = array[0];
+  let max = min;
+  for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) max = array[i];
+      if (array[i] < min) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length || typeof array == 'undefined') return 0;
+  let min = array[0];
+  let max = min;
+  for (let i = 1; i < array.length; i++) {
+      if (array[i] > max) max = array[i];
+      if (array[i] < min) min = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length || typeof array == 'undefined') return 0;
+  let arraySum = 0;
+  for (let i = 0; i < array.length; i++) {
+    arraySum += array[i];
+  }
+  let result = arraySum / array.length;
+  return result;
 }
